a307b26a18cc7cbd30dfd2692f49d3ac3c780061
mubeejoy12 <83483923+mubeejoy12@users.noreply.github.com>
2024-02-16 10:41:57 +0100

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import "./App.css";
+import Boddy from "./components/Body/Boddy";
+import Header from "./components/Header/Header";
+import Form from "./components/form/Form";
+
+function App() {
+  return <>
+        <Header/>
+        <Boddy/>
+        <Form/>
+  </>;
+}
+
+export default App;
